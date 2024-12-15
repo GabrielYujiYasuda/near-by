33ccd1653e9279bd71f5007606af5bbceb1b5f0d
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2024-12-15 12:38:50 -0800

    feat: Adds splash screen

---
+++ app/src/main/java/com/example/nearby/ui/screen/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.example.nearby.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nearby.ui.theme.GreenLight
+import com.example.nearby.R
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier, onNavigateToWelcome: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(GreenLight)
+            .fillMaxWidth()
+    ) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.img_logo_logo_logo_text),
+            contentDescription = "logo image",
+        )
+        Image(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            painter = painterResource(id = R.drawable.bg_splash_screen),
+            contentDescription = "background image"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen(onNavigateToWelcome = { })
+}
