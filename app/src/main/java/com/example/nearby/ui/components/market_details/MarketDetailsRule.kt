27481171cf86795e4c42cb993460beb0eee84ec6
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2024-12-15 11:50:01 -0800

    refact: Changes market components folder

--- app/src/main/java/com/example/nearby/ui/components/market_details/MarketDetailsRule.kt
+++ app/src/main/java/com/example/nearby/ui/components/market_details/MarketDetailsRule.kt
@@ -1,4 +1,4 @@
-package com.example.nearby.ui.market_details
+package com.example.nearby.ui.components.market_details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
