dbfb04e0d30d317f7829e5fb1ff026065c2cef14
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2024-12-27 19:33:27 -0800

    feat: Turns categories mock visible

--- app/src/main/java/com/example/nearby/data/model/mock/MockCategories.kt
+++ app/src/main/java/com/example/nearby/data/model/mock/MockCategories.kt
@@ -2,23 +2,21 @@ package com.example.nearby.data.model.mock
 
 import com.example.nearby.data.model.Category
 
-class MockCategories {
-    val mockCategories = listOf(
-        Category(
-            id = "1",
-            name = "Food"
-        ),
-        Category(
-            id = "2",
-            name = "Cinema"
-        ),
-        Category(
-            id = "3",
-            name = "Drugstore"
-        ),
-        Category(
-            id = "4",
-            name = "Supermarket"
-        )
+val mockCategories = listOf(
+    Category(
+        id = "1",
+        name = "Food"
+    ),
+    Category(
+        id = "2",
+        name = "Cinema"
+    ),
+    Category(
+        id = "3",
+        name = "Drugstore"
+    ),
+    Category(
+        id = "4",
+        name = "Supermarket"
     )
-}
+)
