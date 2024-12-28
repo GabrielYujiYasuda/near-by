18efc941666d565fb65428e1fefb7704560547ac
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2024-12-27 19:35:11 -0800

    refact: Removes unnecessary space

--- app/src/main/java/com/example/nearby/ui/components/category/NearbyCategoryFilterChipList.kt
+++ app/src/main/java/com/example/nearby/ui/components/category/NearbyCategoryFilterChipList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
